18c3a9e1068578d04bf459d3f0e28df3b7a35767
esrat4690 <esratjahan4690@gmail.com>
2025-08-09 14:46:25 +0600

    include error,loading,forbiden page. also add about page

--- src/Pages/Shared/Logo/Logo.jsx
+++ src/Pages/Shared/Logo/Logo.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Link } from "react-router";
-import logo from "../../../assets/logo.png";
+import logo from "../../../assets/logo2.png";
 
 const Logo = () => {
   return (
     <Link to="/">
       <div className="flex items-center gap-2">
-        <img src={logo} alt="" className=" w-8" />
-        <p className="md:text-3xl font-bold text-[#a4b0e9] underline">
-          ResQPet
-        </p>
+        <img src={logo} alt="" className="h-14" />
       </div>
     </Link>
   );
